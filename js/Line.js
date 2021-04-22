71743569cf28cf74c7f6ce061817641b479aa7d5
surajit468 <78489903+surajit468@users.noreply.github.com>
2021-04-22 18:44:12 +0530

    Add files via upload

---
+++ js/Line.js
@@ -0,0 +1,25 @@
+class Line{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.lineObj= Constraint.create(options);
+        World.add(world, this.lineObj);
+    }
+
+      display(){
+          if(this.lineObj.bodyA){
+            var pointA = this.lineObj.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+          }
+        
+    }
+    fly() {this.lineObj.bodyA=null};
+        
+}
